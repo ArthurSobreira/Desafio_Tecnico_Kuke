ab9e7b346fa4e0a4f14082e021423edbbb1c07a5
Arthur <tutusobreira@gmail.com>
2025-01-11 15:22:43 -0300

    feat: Update settings.py

--- backend/movie_catalog/settings.py
+++ backend/movie_catalog/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',   # Django REST framework
+    'corsheaders',      # Django CORS headers
+    'movies',           # Movies app
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',    # Django CORS headers
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",    # Next.js frontend address
 ]
 
 ROOT_URLCONF = 'movie_catalog.urls'
