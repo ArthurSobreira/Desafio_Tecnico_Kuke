1012e7e9c77a170f8e4dcf07007d37946acaa494
Arthur <tutusobreira@gmail.com>
2025-01-11 15:52:10 -0300

    feat: Create Movie model

---
+++ backend/movies/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Model for the movie object
+class Movie(models.Model):
+	name = models.CharField(max_length=255)
+	image_url = models.URLField()
+	description = models.TextField()
+	year = models.IntegerField()
+
+	# Returns the name of the movie
+	def	__str__(self):
+		return self.name
