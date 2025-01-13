38b066021efedba3a81e2022f17491d1efd8027a
Arthur <tutusobreira@gmail.com>
2025-01-13 00:44:12 -0300

    feat: Update admin.py

--- backend/movies/admin.py
+++ backend/movies/admin.py
@@ -6,7 +6,6 @@ from .models import Filme
 class FilmeAdmin(admin.ModelAdmin):
 	list_display = ('nome', 'ano')
 	search_fields = ('nome',)
-	list_filter = ('ano',)
 
 # Register the Filme model
 admin.site.register(Filme, FilmeAdmin)
