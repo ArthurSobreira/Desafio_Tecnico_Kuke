f4db648e0f8041c7a7224061282e5c050553cf3b
Arthur <tutusobreira@gmail.com>
2025-01-14 02:48:58 -0300

    feat: Update Footer component

--- frontend/pages/components/Footer.tsx
+++ frontend/pages/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-center">
           <p className="text-gray-500 dark:text-white">
-            &copy; {new Date().getFullYear()} Arthur Sobreira.
+            &copy; {new Date().getFullYear()} Arthur Sobreira
           </p>
         </div>
       </div>
