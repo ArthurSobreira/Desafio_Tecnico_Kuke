e912720f0f4c6ab311651e8f6faa279cfbe0bdeb
Arthur <tutusobreira@gmail.com>
2025-01-14 03:05:04 -0300

    feat: Update Header component

--- frontend/pages/components/Header.tsx
+++ frontend/pages/components/Header.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+
+// Arrumar  a identação do código
 const Header = () => {
-  return (
-    <header className="bg-white dark:bg-gray-900">
+	return (
+		<header className="bg-white dark:bg-gray-900">
 			<div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
 				<div className="flex h-16 items-center justify-between">
 					<div className="flex-1 md:flex md:items-center md:gap-12">
