310f052cf17889da713a71427485c19061e9d6a2
Arthur <tutusobreira@gmail.com>
2025-01-14 17:43:22 -0300

    feat: Add image domains

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: [
+      'br.web.img2.acsta.net', 
+      'br.web.img3.acsta.net'
+    ],
+  },
 };
 
 export default nextConfig;
