504a417e3775f1f8ff2f57d5ce427f6ea1d19626
Arthur <tutusobreira@gmail.com>
2025-01-14 02:38:51 -0300

    feat: Create Footer component

---
+++ frontend/pages/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white dark:bg-gray-900 mt-auto">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-center">
+          <p className="text-gray-500 dark:text-white">
+            &copy; {new Date().getFullYear()} Arthur Sobreira.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
